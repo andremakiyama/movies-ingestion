871e19182c9932fd34f03f3cffa8ad9ad6417cd8
André Gomes Makiyama <andre_gomes@hotmail.fr>
2019-11-16 18:47:36 -0300

    GCP Functions adaptation

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from flask import Flask, jsonify
-import json
+#chamada {"path":"now_playing", "api_key":"3a759be71aa9bae9de1e0621d0ef7f14"}
+
+
 import module.movies_api_consumption as apirequest
-
-def data_ingestion(request):
-    data = request.get_json()
+import flask
 
 
-    return jsonify(data)
+def movie_ingestion(request):
+    json_request=request.get_json()
+    
+    response = apirequest.getapidata(json_request["path"],json_request["api_key"])
+
+    api_results = ""
+    try:
+        api_results = response.json()['results']
+        if len(api_results) <= 0:
+            print("Dados não encontrados")
+            system.exit(2)
+    except Exception as e:
+        print(e)
+        exit(99)
+    
+    teste = str(api_results[0])
+    return teste
--- module/movies_api_consumption.py
+++ module/movies_api_consumption.py
@@ -1,12 +1,12 @@
 import requests
 
-def getapidata(path,key,log):
+def getapidata(path,key):
     api_string='https://api.themoviedb.org/3/movie/' + path + '?api_key='+key
     print("API Consumption string: " + api_string)
-    log.debug("API Consumption string: " + api_string)
+
     try:
         a= requests.get(api_string)
         return a
     except Exception as e:
-        log.error("e")
+        print(e)
         sys.exit(1)
