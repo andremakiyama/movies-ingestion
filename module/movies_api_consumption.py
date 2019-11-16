7ee09de20f6174b1e98d1ea59fea32d306a697bd
André Gomes Makiyama <andre_gomes@hotmail.fr>
2019-11-16 17:13:36 -0300

    requirements +gitignore

--- module/movies_api_consumption.py
+++ module/movies_api_consumption.py
@@ -1,8 +1,9 @@
 import requests
 
-def getapidata(path,key):
-    api_string='https://api.themoviedb.org/' + path + '?api_key='+key
+def getapidata(path,key,log):
+    api_string='https://api.themoviedb.org/3/movie/' + path + '?api_key='+key
     print("API Consumption string: " + api_string)
+    log.debug("API Consumption string: " + api_string)
     try:
         a= requests.get(api_string)
         return a
