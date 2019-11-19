fd00ae41cf3d9f017581cf629175a4079d0b3bf1
André Gomes Makiyama <andre_gomes@hotmail.fr>
2019-11-18 22:09:14 -0300

    Adapted to CloudFunction

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#chamada {"path":"now_playing", "api_key":"3a759be71aa9bae9de1e0621d0ef7f14"}
-
-
 import module.movies_api_consumption as apirequest
 import flask
 
@@ -8,17 +5,7 @@ import flask
 def movie_ingestion(request):
     json_request=request.get_json()
     
-    response = apirequest.getapidata(json_request["path"],json_request["api_key"])
+    api_results = apirequest.getapidata(json_request["path"],json_request["api_key"])
 
-    api_results = ""
-    try:
-        api_results = response.json()['results']
-        if len(api_results) <= 0:
-            print("Dados não encontrados")
-            system.exit(2)
-    except Exception as e:
-        print(e)
-        exit(99)
-    
-    teste = str(api_results[0])
-    return teste
+    response= '{\"' + json_request["path"] + '\":' + str(api_results) + "}"
+    return response
--- module/movies_api_consumption.py
+++ module/movies_api_consumption.py
@@ -5,8 +5,15 @@ def getapidata(path,key):
     print("API Consumption string: " + api_string)
 
     try:
-        a= requests.get(api_string)
-        return a
+        request= requests.get(api_string)
+        
+        api_results = request.json()['results']
+        
+        if len(api_results) <= 0:
+            print("Dados não encontrados")
+            system.exit(2)
+            
+        return api_results
     except Exception as e:
         print(e)
         sys.exit(1)
