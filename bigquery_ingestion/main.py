6adc129827f997767acda6da30d23fc81235874d
André Gomes Makiyama <andre_gomes@hotmail.fr>
2019-12-04 20:40:17 -0300

    Update main.py

--- bigquery_ingestion/main.py
+++ bigquery_ingestion/main.py
@@ -39,17 +39,17 @@ def insert_into_bigquery(bucket_name, file_name):
     job_config.autodetect = True
     job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
 
-    uri = "gs://"+ bucket_name + "/" + tablename
+    uri = "gs://"+ bucket_name + "/" + file_name
     print("URI: " + uri)
     
     load_job = BQ.load_table_from_uri(
         uri, dataset_ref.table(tablename), job_config=job_config
     )  # API request
-    print("Starting job {}".format(load_job.job_id))
+    print("Starting job {} ".format(load_job.job_id))
 
     load_job.result()  # Waits for table load to complete.
-    print("Job finished.")
+    print("Job finished. ")
 
     destination_table = client.get_table(dataset_ref.table(tablename))
-    print("Loaded {} rows.".format(destination_table.num_rows))
+    print("Loaded {} rows. ".format(destination_table.num_rows))
 
