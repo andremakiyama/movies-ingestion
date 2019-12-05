9ab4530c6d4f8901abaffc478469858264f2e8be
André Gomes Makiyama <andre_gomes@hotmail.fr>
2019-12-04 23:12:22 -0300

    Update movies_api_consumption.py

--- data_capture/module/movies_api_consumption.py
+++ data_capture/module/movies_api_consumption.py
@@ -9,7 +9,7 @@ def getapidata(path,key):
         try:
             api_results = re.sub("}, {", '}\n{', json.dumps(request.json()["results"])[0:-1])
         except Exception as e:
-            print(e)
+            print("Exception: " +e)
             api_results = json.dumps(request.json())
         if len(api_results) <= 0:
             print("Dados não encontrados")
